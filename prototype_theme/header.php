ee80868d5f1e6901510a1da97c1877cc69c9eed1
Mitch Thompson <thomitchell@gmail.com>
2016-07-25 16:33:10 -0700

    Starts breaking theme into template files for wordpress theme

---
+++ prototype_theme/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <title>Musicians of the Houston Grand Opera</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<header>
+    <nav>
+        <img src="images/logo_placeholder.png">
+        <ul class="nav">
+            <li><a href="#">About</a></li>
+            <li><a href="#">Musicians</a></li>
+            <li><a href="#">Perfomances</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul><!--nav-->
+    </nav>
+</header>
+<body>
---
+++ prototype_theme/index.php
@@ -0,0 +1,58 @@
+<?php get_header(); ?>
+    <div id="content" class="home">
+        
+        <div id="banner-box">
+            <div id="banner">
+                <div id="quotes">
+                     <blockquote>"Best orchestra performance I have ever seen!"<span> - The Guardian</span></blockquote>
+                     <a href="#">ABOUT US</a>
+                </div><!--quotes-->
+                <div id="events">
+                    <h2>Upcoming Performances</h2>
+                    <ul>
+                        <li>
+<!--                            <h3>2.13</h3>-->
+                            <h3>LOREM IPSUM DOLOR SIT AMET</h3>
+                            <p>In pretium accumsan suscipit.</p>
+                        </li>
+                        <li>
+<!--                            <h3>2.25</h3>-->
+                            <h3>LOREM IPSUM DOLOR SIT AMET</h3>
+                            <p>Pellentesque volutpat nunc consectetur.</p>
+                        </li>
+                        <li>
+<!--                            <h3>3.11</h3>-->
+                            <h3>LOREM IPSUM DOLOR SIT AMET</h3>
+                            <p>Pellentesque volutpat nunc consectetur.</p>
+                        </li>
+                    </ul>
+                    <a href="#">EVENT CALENDAR</a>
+                </div><!--events-->
+            </div>
+        </div><!--banner-->
+        
+        <div id="posts">
+            <h2 class="heading">LATEST NEWS</h2>
+            <div id="posts-box-outter">
+                <div id="posts-box-inner">
+                    <div id="post1" class="post">
+                        <h2>Lorem Ipsum Dolor</h2>
+                        <p>Nulla ultrices eget nisi sit amet aliquet. In pretium accumsan suscipit. Pellentesque volutpat nunc consectetur. Nulla ultrices eget nisi sit amet aliquet. In pretium accumsan suscipit. </p>
+                        <a href="#">READ MORE</a>
+                    </div><!--post1-->
+                    <div id="post2" class="post">
+                        <h2>Lorem Ipsum Dolor</h2>
+                        <p>Nulla ultrices eget nisi sit amet aliquet. In pretium accumsan suscipit. Pellentesque volutpat nunc consectetur. Nulla ultrices eget nisi sit amet aliquet. In pretium accumsan suscipit. </p>
+                        <a href="#">READ MORE</a>
+                    </div><!--post2-->
+                </div><!--posts-box-inner-->
+                <div id="post3" class="post">
+                    <h2>Lorem Ipsum Dolor</h2>
+                    <p>Nulla ultrices eget nisi sit amet aliquet. In pretium accumsan suscipit. Pellentesque volutpat nunc consectetur. Nulla ultrices eget nisi sit amet aliquet. In pretium accumsan suscipit. </p>
+                    <a href="#">READ MORE</a>
+                </div><!--post3-->
+            </div><!--posts-box-outter-->
+        </div><!--posts-->
+    </div><!--content-->
+    
+<?php get_footer(); ?>
