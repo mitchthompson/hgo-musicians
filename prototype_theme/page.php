283c9720e6bcad38b138a5877a78236b51f45609
Mitch Thompson <thomitchell@gmail.com>
2016-08-08 15:08:31 -0700

    Adds bootstrap container test to page.php

--- prototype_theme/page.php
+++ prototype_theme/page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div id="content" class="page">
+<div id="content" class="page container">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); // start the loop ?>
     <h2><?php the_title(); // get the page or posting title ?></h2> 
     <?php the_content(''); // get page or posting written content ?>
